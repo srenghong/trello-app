4f6353ee7dae11b00b05977023ea7abbacab2756
srenghong <srenghong21@gmail.com>
2020-02-01 09:22:47 +0100

    initial commit

---
+++ src/components/TrelloBoard.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TrelloBoard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Trello Board</h1>
+      </div>
+    );
+  }
+}
